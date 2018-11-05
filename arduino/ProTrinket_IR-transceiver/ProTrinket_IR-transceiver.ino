9bdf205502a479f53704b37f440d185e9655c711
Ben Adams <Ben.Adams@kcc.com>
2018-11-04 21:36:40 -0600

    Scaffolding for smarter beam-break reactions

--- arduino/ProTrinket_IR-transceiver/ProTrinket_IR-transceiver.ino
+++ arduino/ProTrinket_IR-transceiver/ProTrinket_IR-transceiver.ino
@@ -7,11 +7,17 @@
 #include <Wire.h>
 #include <pins_arduino.h>
 
-
 RF24 radio(10, 5); // CE, CSN
 
+#define RGB_R 3
+#define RGB_G 5
+#define RGB_B 6
 #define LED 3
 #define IR_EYE 4
+
+unsigned long lastVisiblePulse = 0;
+int lastState;
+bool beamBroken = false;
 
 void setup() {
   radio.begin();
@@ -35,14 +41,42 @@ void setup() {
     delay(100);
   }
   delay(400);
+
+  lastState = digitalRead(IR_EYE);
 }
 
 void loop() {
-  // FIXME Add low-batt indicator
-  if (digitalRead(IR_EYE)) {
-    digitalWrite(LED, LOW);
+  int state = digitalRead(IR_EYE);
+  if (lastState != state) {
+    if (state == LOW) {  /* IR eye is active-low */
+      lastVisiblePulse = millis(); 
+    }
+    lastState = state;
+  }
+
+  // We should be seeing high-low emitter cycles every 10.5 ms
+  if (millis() - lastVisiblePulse > 15) {
+    if (state == HIGH) {
+      if (! beamBroken) {
+        // Emitter's been missing for a while (and htis is the first
+        // time we're reacting to it)
+        beamBroken = true;
+        /* BEAM BROKEN! WOOP WOOP WOOP DING DING! */
+      }
+    } else {
+      // Emitter's saturating the eye. That's no good either.
+      /* Throw a warning LED */
+    }
   } else {
+    /* Reset the alarm */
+    beamBroken = false;
+  }
+
+  // FIXME Do something prettier
+  if (beamBroken) {
     digitalWrite(LED, HIGH);
+  } else {
+    digitalWrite(LED, LOW);
   }
 }
 
