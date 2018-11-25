a62cef5269d4da219c0e7a40bba9ce9357028a85
Ben Adams <Ben.Adams@kcc.com>
2018-11-24 22:44:05 -0600

    MOAR DEMOS. FANCIER!

--- arduino/ItsyBitsy_Bicolor8x8_demo/ItsyBitsy_Bicolor8x8_demo.ino
+++ arduino/ItsyBitsy_Bicolor8x8_demo/ItsyBitsy_Bicolor8x8_demo.ino
@@ -24,43 +24,114 @@
 //#include "Adafruit_GFX.h"
 #include "BMA_Dual8x8.h"
 
-#define DELAYCONST 15
+#define DELAYCONST 30
 
 BMA_Dual8x8 scr = BMA_Dual8x8();
 void setup() {
   scr.begin();
 }
 
+int bounce_x = 0;
+int bounce_y = 2;
+int direction_x = 1;
+int direction_y = 1;
+int bounce_c = LED_GREEN;
+
 void loop() {
+
+  for (int ch=120; ch<123; ch++) {
+    for (int z=16; z>7; z--) {
+      scr.clear();
+      scr.drawMiniChar(z, 2, ch, LED_GREEN);
+      scr.writeDisplay();
+      delay(DELAYCONST);
+    }
+    delay(DELAYCONST * 8);
+    for (int z=7; z>-4; z--) {
+      scr.clear();
+      scr.drawMiniChar(z, 2, ch, LED_RED);
+      scr.writeDisplay();
+      delay(DELAYCONST);
+    }
+  }
+
+  scr.clear();
+  scr.writeDisplay();
+
+  for (int z=0; z<120; z++) {
+    if ((bounce_x == 0) || (bounce_x == scr._width-1) || (bounce_y == 0) || (bounce_y == scr._height-1)) {
+      bounce_c = LED_RED;
+    } else {
+      bounce_c = LED_GREEN;
+    }
+
+    scr.clear();
+    scr.drawPixel(bounce_x, bounce_y, bounce_c);
+    scr.writeDisplay();
+    delay(DELAYCONST);
+
+    bounce_x += direction_x;
+    bounce_y += direction_y;
+
+    if ((bounce_x < 0) || (bounce_x >= scr._width)) {
+      direction_x *= -1;
+      bounce_x += direction_x * 2;
+    }
+    if ((bounce_y < 0) || (bounce_y >= scr._height)) {
+      direction_y *= -1;
+      bounce_y += direction_y * 2;
+    }
+  }
+
+  scr.clear();
+  scr.writeDisplay();
+
   for (int y=0; y<8; y += 2) {
     for (int x=15; x>=0; x--) {
       scr.greenbuffer[y] |= 1<<x;
       scr.writeDisplay();
       delay(DELAYCONST);
     }
+    scr.clear();
+    scr.writeDisplay();
     for (int x=15; x>=0; x--) {
       scr.redbuffer[y+1] |= 1<<x;
       scr.writeDisplay();
       delay(DELAYCONST);
     }
+    scr.clear();
+    scr.writeDisplay();
   }
 
-  for (int x=15; x>0; x-=2) {
-    for (int y=0; y<8; y++) {
-      scr.greenbuffer[y] |= 1<<x;
-      scr.writeDisplay();
-      delay(DELAYCONST);
+  /*
+  for (int z=0; z<8; z++) {
+    for (int i=0; i<8; i++) {
+      scr.greenbuffer[i] = 0x00FF;
+      scr.redbuffer[i] = 0x00FF;
     }
-    for (int y=0; y<8; y++) {
-      scr.redbuffer[y] |= 1<<(x-1);
-      scr.writeDisplay();
-      delay(DELAYCONST);
-    }
-  }
+    scr.writeDisplay();
+    delay(35);
 
-  for (int i=0; i<8; i++) {
-    scr.greenbuffer[i] = 0;
-    scr.redbuffer[i] = 0;
+    for (int i=0; i<8; i++) {
+      scr.greenbuffer[i] = 0;
+      scr.redbuffer[i] = 0;
+    }
+    scr.writeDisplay();
+    delay(35);
+
+    for (int i=0; i<8; i++) {
+      scr.greenbuffer[i] = 0xFF00;
+      scr.redbuffer[i] = 0xFF00;
+    }
+    scr.writeDisplay();
+    delay(35);
+
+    for (int i=0; i<8; i++) {
+      scr.greenbuffer[i] = 0;
+      scr.redbuffer[i] = 0;
+    }
+    scr.writeDisplay();
+    delay(35);
   }
-  scr.writeDisplay();
+  */
 }
