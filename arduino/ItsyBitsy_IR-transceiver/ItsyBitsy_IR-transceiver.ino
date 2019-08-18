d0d6859f6bd4411aa17d5931b0feecff43859757
Ben Adams <Ben.Adams@kcc.com>
2019-08-18 11:04:23 -0500

    Futzing with voltage reading values

--- arduino/ItsyBitsy_IR-transceiver/ItsyBitsy_IR-transceiver.ino
+++ arduino/ItsyBitsy_IR-transceiver/ItsyBitsy_IR-transceiver.ino
@@ -13,11 +13,11 @@ RF24 radio(3, 2); // CE, CSN
 #define PIEZO1 A1
 #define PIEZO2 A3
 #define IRSENSE 7
-#define LED1 13
-#define LED2 12
+#define LED1 9
+#define LED2 10
 #define LED3 11
-#define LED4 10
-#define LED5 9
+#define LED4 12
+#define LED5 13
 
 // Utilize buzzer and LEDs at startup to aid testing, then
 // go dark & silent to conserve battery power. This mode is
@@ -31,7 +31,26 @@ RF24 radio(3, 2); // CE, CSN
 volatile unsigned long lastVisiblePulse = 0;
 volatile int irstate = HIGH;
 bool beamBroken = false;
+bool lastBeamState = beamBroken;
 bool saturated = false;
+
+void ledwalk() {
+  digitalWrite(LED1, HIGH);
+  delay(300);
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, HIGH);
+  delay(300);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, HIGH);
+  delay(300);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED4, HIGH);
+  delay(300);
+  digitalWrite(LED4, LOW);
+  digitalWrite(LED5, HIGH);
+  delay(300);
+  digitalWrite(LED5, LOW);
+}
 
 void piezoBeep(int duration) {
   for (int i=0; i<duration*5; i++) {
@@ -47,13 +66,28 @@ void piezoBeep(int duration) {
 void showBatteryLevel() {
   int battvoltage = analogRead(BATTSENSE);
 
+  /*
+  if (battvoltage < 10) {
+    digitalWrite(LED1, HIGH);
+  }
+  else if (battvoltage > 1000) {
+    digitalWrite(LED5, HIGH);
+  }
+  delay(1000);
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED5, LOW);
+  delay(1000);
+  */
+
+
   /* Normal scale for analogRead means max reading (1023) == 3.3 VDC.
      But we're reading through a 50% resistor matrix, so 1023 == 6.6 VDC.
      Therefore for a lithium-ion cell, our "acceptable" range is:
         574 == 3.7 VDC
         652 == 4.2 VDC  */
 
-  if ((battvoltage < 543) || (battvoltage > 667)) {  // Outside normal params
+  //if ((battvoltage < 543) || (battvoltage > 667)) {  // Outside normal params
+  if ((battvoltage < 543) || (battvoltage > 1020)) {  // Outside normal params
     for (int i=0; i<3; i++) {
       digitalWrite(LED2, HIGH);
       digitalWrite(LED4, HIGH);
@@ -64,21 +98,32 @@ void showBatteryLevel() {
     }
   }
   else if (battvoltage < 574) { // < 3.7 VDC: Crit low
-    for (int i=0; i<3; i++) {
+    for (int i=0; i<5; i++) {
       digitalWrite(LED1, HIGH);
-      piezoBeep(400);
+      piezoBeep(200);
       digitalWrite(LED1, LOW);
-      delay(400);
+      delay(200);
     }
   }
   else { // 3.7 - 4.3 VDC: Normal
     digitalWrite(LED1, HIGH);                        // >= 3.7 VDC
+    /*
     if (battvoltage > 590) digitalWrite(LED2, HIGH); // >= 3.8 VDC
     if (battvoltage > 606) digitalWrite(LED3, HIGH); // >= 3.9 VDC
     if (battvoltage > 622) digitalWrite(LED4, HIGH); // >= 4.0 VDC
     if (battvoltage > 638) digitalWrite(LED5, HIGH); // >= 4.1 VDC
+    */
+    if (battvoltage > 660) digitalWrite(LED2, HIGH); // >= 3.8 VDC
+    if (battvoltage > 665) digitalWrite(LED3, HIGH); // >= 3.9 VDC
+    if (battvoltage > 670) digitalWrite(LED4, HIGH); // >= 4.0 VDC
+    if (battvoltage > 675) digitalWrite(LED5, HIGH); // >= 4.1 VDC
   }
   delay(1000);
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED4, LOW);
+  digitalWrite(LED5, LOW);
 }
 
 void setupIRint() {
@@ -96,12 +141,19 @@ void irIntFire() {
   }
 }
 
-void radioBroadcast() {
+void radioBroadcast(bool broken) {
   radio.stopListening();
-  // Sentence construction: [Ir unit][unit#]
-  char xmit[2];
+  // Sentence construction: [Ir unit][unit#][
+  char xmit[3];
   xmit[0] = 'I';
   xmit[1] = '1'; // Different value for every transceiver
+  if (broken) {
+    xmit[2] = '1';
+  }
+  else {
+    xmit[2] = '0';
+  }
+
   radio.write(&xmit, sizeof(xmit));
   radio.startListening();
 }
@@ -122,11 +174,15 @@ void setup() {
   pinMode(LED3, OUTPUT); digitalWrite(LED3, LOW);
   pinMode(LED4, OUTPUT); digitalWrite(LED4, LOW);
   pinMode(LED5, OUTPUT); digitalWrite(LED5, LOW);
+  pinMode(BATTSENSE, INPUT);
   pinMode(PIEZO1, OUTPUT);
   pinMode(PIEZO2, OUTPUT);
 
+  ledwalk();
+  delay(1000);
+
   showBatteryLevel();
-  delay(400);
+  delay(1000);
 
 }
 
@@ -165,14 +221,17 @@ void loop() {
     digitalWrite(LED5, LOW);
   }
 
-  if (beamBroken) {
-    radioBroadcast();
-    if (millis() < TESTMODE_MS) {
-      digitalWrite(LED3, HIGH);
-      piezoBeep(800);
+  if (lastBeamState != beamBroken) {
+    radioBroadcast(beamBroken);
+    if (beamBroken) {
+      if (millis() < TESTMODE_MS) {
+        digitalWrite(LED3, HIGH);
+        piezoBeep(800);
+      }
+    } else {
+      digitalWrite(LED3, LOW);
     }
-  } else {
-    digitalWrite(LED3, LOW);
+    lastBeamState = beamBroken;
   }
 }
 
