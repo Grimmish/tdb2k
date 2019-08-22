b75f52d347d897665befcd53ac2a0e7ce3c45c44
Ben Adams <ben.ma.adams@gmail.com>
2019-08-21 21:59:00 -0500

    Fancier!

--- test_stubs/headunit-v2-sendtest.py
+++ test_stubs/headunit-v2-sendtest.py
@@ -8,24 +8,17 @@ from rf24_headunit import rf24_headunit
 radio = bens_rf24(debug=False)
 headunit = rf24_headunit(radio=radio, addr=0xE1E1E1E1E1)
 
-headunit.radio.set_tx_pipeline(headunit.addr)
-headunit.radio.w_tx_payload([ord('D'), ord('R'), 0b00001000, 0b00000000,
-                                                 0b00010100, 0b00000000,
-                                                 0b00100010, 0b00000000,
-                                                 0b01000001, 0b00000001,
-                                                 0b10000000, 0b10000010,
-                                                 0b00000000, 0b01000100,
-                                                 0b00000000, 0b00101000,
-                                                 0b10100101, 0b00010000 ])
-
-headunit.radio.w_tx_payload([ord('D'), ord('G'), 0b00010000, 0b00000000,
-                                                 0b00101000, 0b00000000,
-                                                 0b01000100, 0b00000001,
-                                                 0b10000010, 0b00000010,
-                                                 0b00000001, 0b00000100,
-                                                 0b00000000, 0b10001000,
-                                                 0b00000000, 0b01010000,
-                                                 0b10100101, 0b00100000 ])
+for m in range(100):
+  for x in range(8):
+    for z in range(8):
+      if x == z:
+        headunit.fb['R'][z] = 0xFF
+        headunit.fb['G'][z] = 0xFF<<8
+      else:
+        headunit.fb['R'][z] = 0
+        headunit.fb['G'][z] = 0
+    headunit.sendBuffer()
+    time.sleep(0.1)
 
 radio.destroy()
 print("SPI closed")
