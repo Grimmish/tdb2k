3c05b59733d7fa3736e4b0ec7315562f08eb45e9
Ben Adams <Ben.Adams@kcc.com>
2019-08-21 21:34:47 -0500

    Updated display lib for 16x8 bi-color display

--- modules/rf24_headunit.py
+++ modules/rf24_headunit.py
@@ -4,18 +4,26 @@ class rf24_headunit():
   def __init__(self, radio, addr=0xE7E7E7E7E7):
     self.radio = radio
     self.addr = addr
-    
-  def display(self, payload):
-    as_bytes = [ord('D')] + map(lambda x: ord(x), list(str(payload)))
-    self.radio.set_tx_pipeline(self.addr)
-    #FIXME-AUTOMATIC-NOW self.radio.set_tx_mode()
-    return self.radio.w_tx_payload(as_bytes)
 
-  def led(self, color):
-    as_bytes = [ord('L'), ord(color[0].upper())]
+class rf24_headunit():
+  def __init__(self, radio, addr=0xE7E7E7E7E7):
+    self.radio = radio
+    self.addr = addr
+
+    # Framebuffer is eight rows, each row representing
+    # 16 bits of width. Two color channels.
+    self.fb = { 'R': [0,0,0,0,0,0,0,0],
+                'G': [0,0,0,0,0,0,0,0] }
+
+  # Break the 16-bit framebuffer rows into 8-bit words
+  def fbreducer(c, i):
+    c.extend([i>>8, i & 0xFF]
+    return c
+
+  def display(self, payload):
     self.radio.set_tx_pipeline(self.addr)
-    #FIXME-AUTOMATIC-NOW self.radio.set_tx_mode()
-    return self.radio.w_tx_payload(as_bytes)
+    rchan = [ord('D'), ord('R')] + reduce(self.fbreducer, self.fb['R'], [])
+    gchan = [ord('D'), ord('R')] + reduce(self.fbreducer, self.fb['G'], [])
+    return self.radio.w_tx_payload(rchan) and self.radio.w_tx_payload(gchan)
 
 ##########
-
