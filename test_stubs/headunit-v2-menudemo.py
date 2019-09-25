6635cf578a8e457e12acb0dd75c4747723aa5e6f
Ben Adams <ben.ma.adams@gmail.com>
2019-09-24 21:14:14 -0500

    Add showfunction example

--- test_stubs/headunit-v2-menudemo.py
+++ test_stubs/headunit-v2-menudemo.py
@@ -13,6 +13,11 @@ def changePixmap(menuitem, args):
   menuitem['labeltype'] = 'pixart'
   return menuitem
 
+def changeToSecs(menuitem, args):
+  menuitem['label'] = time.strftime('%S', time.localtime())
+  menuitem['labeltype'] = 'string'
+  return menuitem
+
 radio = bens_rf24(addr=0xE0E0E0E0E0, debug=False)
 radio.set_rx_pipeline(chan=0, enable=1, addr=0xE0E0E0E0E0)
 
@@ -22,11 +27,30 @@ headunit.radio.set_rx_mode()
 headunit.menu.tree[1]['dofunction'] = changePixmap
 headunit.menu.tree[1]['do_args'] = []
 
+headunit.menu.tree[0]['submenu'][1]['showfunction'] = changeToSecs
+headunit.menu.tree[0]['submenu'][1]['show_args'] = []
+
 headunit.drawCurrentMenuItem()
+
+deadswitch = time.time()
 
 print("Waiting forever... (use CTRL+C to break out)")
 while True:
+  time.sleep(0.01)
+
+  if (time.time() - deadswitch > 2.3):
+    print "DEAD SWITCH! No contact in 2.3 seconds. REBOOT THE RADIO!"
+    radio = bens_rf24(addr=0xE0E0E0E0E0, debug=False)
+    radio.set_rx_pipeline(chan=0, enable=1, addr=0xE0E0E0E0E0)
+    headunit = rf24_headunit(radio=radio, addr=0xE1E1E1E1E1)
+    headunit.radio.set_rx_mode()
+    headunit.menu.tree[1]['dofunction'] = changePixmap
+    headunit.menu.tree[1]['do_args'] = []
+    headunit.drawCurrentMenuItem()
+    deadswitch = time.time()
+
   if headunit.radio.rx_dr():
+    deadswitch = time.time()
     packet = "".join(map(chr, headunit.radio.r_rx_payload()))
     print("Message!: " + packet)
     if packet == 'P0':
